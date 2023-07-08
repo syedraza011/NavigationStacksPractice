7a7a0a87586224db1dad2d67a764130e028ef47a
Syed Raza <syed.raza011@gmail.com>
2023-07-08 09:57:09 -0400

    Initial Commit

---
+++ NavigationStacksPractice/NavigationStacksPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStacksPracticeApp.swift
+//  NavigationStacksPractice
+//
+//  Created by Syed Raza on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStacksPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
